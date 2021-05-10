46bfb430deb8f30ef7e957de61b5c38b8df6ab15
ebiw-gobinda <gobinda@ebiw.com>
2021-05-10 11:10:32 +0530

    2nd commit

--- starter/script.js
+++ starter/script.js
@@ -21,7 +21,7 @@ document.querySelector('.check').addEventListener('click',function(){
     }else if(guess<secretNumber){
         if (score>1){
             document.querySelector('.message').textContent='no is too short'
-        score--;
+            score--;
         document.querySelector('.score').textContent=score;
         }else {
             document.querySelector('.message').textContent='you lost the game';
